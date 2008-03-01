669a8ffa13bf79c0eafdc92a2d0e948f974b329d
Marc Schinnenburg <marc@schinnenburg.com>
2008-03-01 14:13:46 +0100

    new configuration model

--- PyConfig/openwns/PyConfig.py
+++ PyConfig/openwns/PyConfig.py
@@ -98,6 +98,216 @@ class View(dict):
                 result += ' '*indentSize
         return result[:-indentSize].rstrip("\n")
 
+class IAttributeHandler(property):
+    # we are a property ourselfs :)
+    def __init__(self, getter=None, setter=None, deller=None, doc=None):
+        super(IAttributeHandler, self).__init__(getter, setter, deller, doc)
+
+    """ MetaConf uses these to inject the necessary information """
+    def setAttributeName(self, attrName):
+        self._attributeName = attrName
+
+    def setClassName(self, className):
+        self._className = className
+
+class MetaConf(type):
+
+    def __new__(cls, className, bases, dct):
+        # cls == MetaConf
+        if not dct.has_key("__attributes__"):
+            dct["__attributes__"] = []
+        for attributeName, attribute in dct.items():
+            if isinstance(attribute, IAttributeHandler):
+                # inject the name of the attribute and the name of the class
+                attribute.setAttributeName(attributeName)
+                attribute.setClassName(className)
+                dct["__attributes__"].append(attributeName)
+        return type.__new__(cls, className, bases, dct)
+
+    def __init__(cls, className, bases, dct):
+        super(MetaConf, cls).__init__(className, bases, dct)
+
+
+class Config(object):
+
+    class NoSuchAttribute(Exception):
+        """ This exception is thrown if an attribute is accessed that is
+        not available """
+
+        def __init__(self, obj, name):
+            """ The constructor takes the object where the attribute was
+            missing and the name of the attribute. It prints all allowed attributes"""
+
+            cls = obj.__class__
+            error = "\nAttribute '" + name + "' not allowed in '" + cls.__name__ + "'"
+            error += "\nAllowed attributes are:"
+            for allowedAttribute in obj.__attributes__:
+                error += "\n  " + allowedAttribute
+            super(Config.NoSuchAttribute, self).__init__(error)
+
+    __metaclass__ = MetaConf
+
+    # this is used by the params to store the actual instances
+    __attributeValues = None
+
+    def __setattr__(self, attr, value):
+        try:
+            getattr(self, attr) # raises an exception in case attr is not known
+        except AttributeError:
+            raise Config.NoSuchAttribute(self, attr)
+        # we didn't find it in the alloweed attributes list
+        object.__setattr__(self, attr, value)
+
+    def __new__(cls):
+        obj = object.__new__(cls)
+        obj.__attributeValues = {}
+        return obj
+
+    @classmethod
+    def getAttributeHandler(cls, name):
+        return cls.__dict__[name]
+
+    def isAttributeSet(self, name):
+        return self.__attributeValues.has_key(name)
+
+    def getAttribute(self, name):
+        return self.__attributeValues[name]
+
+    def setAttribute(self, name, value):
+        self.__attributeValues[name] = value
+
+
+class Parameter(IAttributeHandler):
+    class NoValueSet(Exception):
+        def __init__(self):
+            super(Parameter.NoValueSet, self).__init__("No value is set!")
+
+    def __init__(self, doc, *hooks):
+        super(Parameter, self).__init__(self.getter, self.setter)
+        # convert to string, __doc__ MUST be a string
+        self.__doc__ = str(doc)
+        self.hooks = hooks
+        # hooks can implement, if they implement it, they will be
+        # asked to provide something. they all get: the
+        # attributeValue, the AtrributeHandler and the object where to
+        # set the attribute. Available hooks are:
+        # onAttributeGetButNotSet()
+        # beforeAttributeSet()
+        # afterAttributeGet()
+
+    def setter(self, obj, attributeValue):
+        """ The setter takes exactly two parameters: the object for
+        which the parameter should be set and the value to be set """
+
+        assert isinstance(obj, Config)
+        for hook in self.hooks:
+            if hasattr(hook, "beforeAttributeSet"):
+                attributeValue = hook.beforeAttributeSet(attributeValue, self, obj)
+        obj.setAttribute(self._attributeName, attributeValue)
+
+    def getter(self, obj):
+        """ The getter takes exactly one parameters: the object from
+        which the parameter should be fetched"""
+
+        assert isinstance(obj, Config)
+        if obj.isAttributeSet(self._attributeName):
+            attributeValue = obj.getAttribute(self._attributeName)
+            for hook in self.hooks:
+                if hasattr(hook, "afterAttributeGet"):
+                    attributeValue = hook.afterAttributeGet(attributeValue, self, obj)
+            return attributeValue
+        else:
+            for hook in self.hooks:
+                # the first hook that matches is fine!
+                if hasattr(hook, "onAttributeGetButNotSet"):
+                    attributeValue = hook.onAttributeGetButNotSet(self, obj)
+                    # call afterAttributeGet hooks
+                    for hook in self.hooks:
+                        if hasattr(hook, "afterAttributeGet"):
+                            attributeValue = hook.afterAttributeGet(attributeValue, self, obj)
+                    return attributeValue
+            raise Parameter.NoValueSet()
+
+
+
+class RangeCheck(object):
+    class Error(Exception):
+        def __init__(self, cls, name, start, stop, value, doc):
+            error =  "\nRangeCheck.Error for for '%s.%s'" % (cls, name)
+            error += "\n  range: [%f, %f]" % (start, stop)
+            error += "\n  value: '%f'" % (value)
+            error += "\n    doc: %s" % (doc)
+            super(RangeCheck.Error, self).__init__(error)
+
+    def __init__(self, start, stop):
+        self.start = start
+        self.stop = stop
+        self.afterAttributeGet = self.beforeAttributeSet
+
+    def beforeAttributeSet(self, attributeValue, attributeHandler, obj):
+        if not (attributeValue >= self.start and attributeValue <= self.stop):
+            raise RangeCheck.Error(
+                attributeHandler._className,
+                attributeHandler._attributeName,
+                self.start,
+                self.stop,
+                attributeValue,
+                attributeHandler.__doc__)
+        return attributeValue
+
+
+class TypeCheck(object):
+    class Error(Exception):
+        def __init__(self, cls, name, paramType, value, doc):
+            error =  "\nTypeCheck.Error for '%s.%s'" % (cls, name)
+            error += "\n  expected: '%s'" % (paramType.__name__)
+            error += "\n       got: '%s'" % (type(value).__name__)
+            error += "\n       doc: %s" % (doc)
+            super(TypeCheck.Error, self).__init__(error)
+
+    def __init__(self, theType):
+        self.theType = theType
+        self.afterAttributeGet = self.beforeAttributeSet
+
+    def beforeAttributeSet(self, attributeValue, attributeHandler, obj):
+        if not isinstance(attributeValue, self.theType):
+            raise TypeCheck.Error(
+                attributeHandler._className,
+                attributeHandler._attributeName,
+                self.theType,
+                attributeValue,
+                attributeHandler.__doc__)
+        return attributeValue
+
+class Default(object):
+    def __init__(self, default):
+        self.value = default
+
+    def onAttributeGetButNotSet(self, attributeHandler, obj):
+        return self.value
+
+class TypedParameter(Parameter):
+
+    def __init__(self, doc, type, default):
+        self.default = Default(default)
+        super(TypedParameter, self).__init__(doc, TypeCheck(type), self.default)
+
+    def setDefault(self, default):
+        self.default.value = default
+
+class Int(TypedParameter):
+    def __init__(self, doc, default):
+        super(Int, self).__init__(doc, int, default)
+
+class String(TypedParameter):
+    def __init__(self, doc, default):
+        super(String, self).__init__(doc, str, default)
+
+class Float(TypedParameter):
+    def __init__(self, doc, default):
+        super(Float, self).__init__(doc, float, default)
+
+
 
 class PyConfigTest:
     foo = 42
