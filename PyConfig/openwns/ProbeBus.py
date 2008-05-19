cd82910a3410f55869d6ff321f7ac4580e2ecff4
Daniel Bueltmann <dbn@comnets.rwth-aachen.de>
2008-05-19 06:17:08 -0400

    Some cleanup in ProbeBus configuration

--- PyConfig/openwns/ProbeBus.py
+++ PyConfig/openwns/ProbeBus.py
@@ -25,44 +25,9 @@
 #
 ###############################################################################
 
-import openwns.logger
 import openwns
-import copy
-from openwns.pyconfig import attrsetter
-
-# class SubTreeRegistry(object):
-
-#         def __init__(self):
-#                 super(SubTreeRegistry, self).__init__()
-#                 self.subtrees = []
-
-#         def insertSubTree(self, _subtree):
-# 		assert isinstance(_subtree, SubTree)
-#                 self.subtrees.append(_subtree)
-
-#         def removeSubTree(self, _subtree):
-# 		assert isinstance(_subtree, SubTree)
-#                 self.subtrees.remove(_subtree)
-
-#         def insertSubTrees(self, _subtrees):
-# 		for tree in _subtrees:
-# 			assert isinstance(tree, SubTree)
-# 			self.subtrees.append(tree)
-
-#         def insertProbeBus(self, _probeBusID, _probeBus):
-# 		s = SubTree(_probeBusID)
-# 		s.top.append(_probeBus)
-# 		self.subtrees.append(s)
-
-# 	def clear(self):
-# 		self.subtrees = []
-
-# 	def getSubTree(self, name):
-# 		for subtree in self.subtrees:
-# 			if subtree.probeBusID == name:
-# 				return subtree
-# 		raise Exception("'"+name+"' not found in "+str(self))
-
+import openwns.logger
+import openwns.pyconfig.attrsetter
 
 class MeasurementSource(object):
 
@@ -72,6 +37,15 @@ class MeasurementSource(object):
 
 	def addObserver(self, probeBus):
 		self.observers.append(probeBus)
+		return probeBus
+
+class ProbeBus(MeasurementSource):
+
+	def __init__(self):
+		MeasurementSource.__init__(self)
+
+	def observe(self, probeBus):
+		probeBus.addObserver(self)
 		return probeBus
 
 class ProbeBusRegistry(object):
@@ -86,129 +60,6 @@ class ProbeBusRegistry(object):
 			self.measurementSources[probeBusID] = MeasurementSource()
 
 		return self.measurementSources[probeBusID]
-
-class ProbeBus(MeasurementSource):
-
-	def __init__(self):
-		MeasurementSource.__init__(self)
-
-	def observe(self, probeBus):
-		probeBus.addObserver(self)
-		return probeBus
-
-# class ProbeBus:
-#         """ Base configuration class for all probe busses. It keeps track of
-#         the observing relationship between probe busses."""
-#         def __init__(self, outputTag):
-#                 self.subject = None
-#                 self.observers = []
-# 		self.outputTag = outputTag
-
-#         def observe(self, otherProbeBus):
-# 		assert self.subject == None, "I am already observing another subject: "+str(self.subject)
-# 		self.subject = otherProbeBus
-#                 otherProbeBus.__addObserver(self)
-
-# 	def listen(self, otherProbeBus):
-# 		""" listen behaves like observe but returns the otherProbeBus. You can use this for chaining ProbeBusses, e.g. if
-#                     you want to log measurements on an already defined ProbeBus you can simply wrap the instatiation of this ProbeBus
-#                     with a LoggingProbeBus like this:
-# 		      # Without Logging
-# 		      myProbeBus = MasterProbeBus()
-
-# 		      # With Logging
-# 		      myProbeBus = LoggingProbeBus().listen(MasterProbeBus())
-# 	        """
-# 		self.observe(otherProbeBus)
-# 		return otherProbeBus
-
-#         def __addObserver(self, observer):
-#                 self.observers.append(observer)
-
-# 	def getOutputTags(self):
-# 		tags = []
-# 		if self.subject != None:
-# 			tags += self.subject.getOutputTags()
-
-# 		tags.append(self.outputTag)
-# 		return tags
-
-# class SubTree:
-# 	"""
-# 	Class that represents and handles subtrees of a probebus
-# 	hierarchy. The leafs are instances of ProbeBusses. This is a
-# 	management structure.
-# 	"""
-# 	probeBusID = None
-# 	top = None
-
-# 	def __init__(self, _probeBusID = ""):
-# 		self.probeBusID = _probeBusID
-# 		self.top = []
-
-#         def addToTop(self, busses):
-#                 """ takes a list or one instance of a probe bus """
-#                 if not isinstance(busses, list):
-#                         busses = [busses]
-#                 for bus in busses:
-#                         if not isinstance(bus, ProbeBus):
-#                                 raise Exception("Only instances of ProbeBus allowed")
-#                         self.top.append(bus)
-
-
-# 	def empty(self):
-# 		return self.top == []
-
-# 	def getBottom(self):
-# 		def getLeafs(probeBus):
-# 			if probeBus.observers == []:
-# 				yield probeBus
-# 			else:
-# 				for bus in probeBus.observers:
-# 					for item in getLeafs(bus):
-# 						yield item
-# 		bottom = []
-# 		for upper in self.top:
-# 			for bus in getLeafs(upper):
-# 				bottom.append(bus)
-# 		return bottom
-
-# 	def chain(self, other):
-# 		""" Adds deep copies of other to the leafs of self"""
-
-# 		if self.empty():
-# 			other.probeBusID = self.probeBusID
-# 			return other
-# 		if other.empty():
-# 			return self
-# 		assert isinstance(other, SubTree), "Can only chain other subtrees!"
-# 		newBottom = []
-# 		for b in self.getBottom():
-# 			newChild = copy.deepcopy(other)
-# 			newChild.__observe(b)
-# 			newBottom += newChild.getBottom()
-# 		s = SubTree(self.probeBusID)
-# 		s.top = self.top
-# 		return s
-
-# 	def dump(self):
-# 		def recursiveWalk(probeBus, currentLevel=0):
-# 			yield probeBus, currentLevel
-# 			for bus in probeBus.observers:
-# 				for item, level in recursiveWalk(bus, currentLevel+1):
-# 					yield item, level
-
-# 		print "SubTree: '"+self.probeBusID+"'"
-# 		for upper in self.top:
-# 			for bus, level in recursiveWalk(upper):
-# 				indentation = ""
-# 				for ii in xrange(level): indentation += "  "
-# 				print "%s%d: %s %s" % (indentation, level, str(bus), str(bus.getOutputTags()))
-
-# 	def __observe(self, parent):
-# 		# plug all the top busses into the parent
-# 		for bus in self.top:
-# 			bus.observe(parent)
 
 class PassThroughProbeBus(ProbeBus):
         """ The PassThroughProbeBus always accepts and always forwards. """
@@ -280,5 +131,5 @@ class LogEvalProbeBus(ProbeBus):
         def __init__(self, **kwargs):
 		ProbeBus.__init__(self)
 		outputDir = openwns.getSimulator().outputDir
-		attrsetter(self, kwargs)
+		openwns.pyconfig.attrsetter(self, kwargs)
 
