4a407afc4dc77a312c445c68edac5e21598c4366
Benedikt Wolz <bmw@comnets.rwth-aachen.de>
2011-05-18 15:03:47 +0200

    bugfix: over write grouper defined values

--- src/scheduler/strategy/Strategy.cpp
+++ src/scheduler/strategy/Strategy.cpp
@@ -870,6 +870,8 @@ Strategy::doAdaptiveResourceScheduling(RequestForResource& request,
         resultMapInfoEntry->estimatedCQI.interference = estimatedCandI.I;
         resultMapInfoEntry->estimatedCQI.pathloss = estimatedCandI.PL;
         resultMapInfoEntry->estimatedCQI.sdma.iIntra = estimatedCandI.sdma.iIntra;
+        resultMapInfoEntry->txPower = txPower * 1.0 / currentGroup.size();
+        resultMapInfoEntry->phyModePtr = colleagues.registry->getPhyModeMapper()->getBestPhyMode(estimatedCandI.C / estimatedCandI.I);
 
         assure(antennaPattern != wns::service::phy::ofdma::PatternPtr(),"invalid pattern");
 
